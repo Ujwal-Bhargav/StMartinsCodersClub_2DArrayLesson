259243ab57ff572526f2d244e75390bf917de85f
Ujwal-Bhargav <ujwalhanuman@gmail.com>
2018-11-04 19:32:22 +0530

    Add files via upload

---
+++ Assignment2.c
@@ -0,0 +1,26 @@
+/*
+
+    Given, a square matrix  return absolute difference of sum of diagonals(left and right)
+
+        Ex:
+                1 2 3
+                4 5 6
+                7 8 9
+            Here,
+                (L)left diagonal sum is (1+5+9)=12
+                (R)right diagonal sum is (3+5+7)=15
+                Absolute difference(|L-R|)=|12-15|=3
+
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    /*
+    Allocate memory dynamically and do write code that works for all input(Different size 3,4,5....)
+    */
+    return 0;
+}
